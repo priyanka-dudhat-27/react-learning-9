6b30440e244b6e0e18939bc6c7c6336020573321
pdudhat27 <pdudhat27@gmail.com>
2024-04-29 10:17:41 +0530

    order/unorder list completed

---
+++ Day-6/pagination/src/component/Home.jsx
@@ -0,0 +1,15 @@
+import React,{ useEffect} from 'react'
+
+const Home = () => {
+
+    useEffect({
+        
+    })
+  return (
+    <div>
+       <h2>Home page</h2> 
+    </div>
+  )
+}
+
+export default Home
