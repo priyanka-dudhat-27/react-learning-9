3fbfdf2c1b55aa5e155fec04dc8a52f65cbb5ddd
priyanka-dudhat-27 <pdudhat27@gmail.com>
2024-05-02 08:27:49 +0530

    create

---
+++ Day-13/crud_create/src/pages/Home.jsx
@@ -0,0 +1,80 @@
+/* eslint-disable no-unused-vars */
+import React, { useState,useEffect } from "react";
+
+const Home = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [users, setUsers] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    fetch("http://localhost:8080/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: name,
+        email: email,
+        password: password,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        // setUsers([...users, data]); // Add the new user to the existing users array
+        alert("Data added successfully");
+        setName("");
+        setEmail("");
+        setPassword("");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(()=>{
+    setUsers(users);
+  },[])
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="name"
+          value={name}
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
+        <br />
+        <br />
+        <input
+          type="email"
+          placeholder="email"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+        <br />
+        <br />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+        <br />
+        <br />
+        <input type="submit" />
+      </form>
+    </div>
+  );
+};
+
+export default Home;
