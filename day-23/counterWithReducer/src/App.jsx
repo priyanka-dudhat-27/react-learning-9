be133f37c83f55532827d97172b95baf5344585c
pdudhat27 <pdudhat27@gmail.com>
2024-05-30 10:19:20 +0530

    counter app with usereducer hook

---
+++ day-23/counterWithReducer/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import Counter from './pages/Counter'
+
+const App = () => {
+  return (
+    <div>
+      <Counter/>
+    </div>
+  )
+}
+
+export default App
