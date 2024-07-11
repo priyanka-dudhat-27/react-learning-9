be0a09fa88ed3e71994b54bcf9a63e4eef46ea8e
priyanka-dudhat-27 <pdudhat27@gmail.com>
2024-07-12 00:15:40 +0530

    data show in table done

---
+++ day-31/data_table/src/App.jsx
@@ -0,0 +1,49 @@
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from 'react';
+import './App.css';
+
+const App = () => {
+  const [users, setUsers] = useState([]);
+
+  const fetchUser = async () => {
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/users');
+      const data = await response.json();
+      setUsers(data);
+    } catch (error) {
+      console.error('Error fetching users:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  return (
+    <div>
+      <h1>React Data Table</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Username</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map(user => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td>{user.username}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default App;
